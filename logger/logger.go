5bf05b4409c67578d28e805458f42f9b6550e053
LeeSeika <leeseika@outlook.com>
2024-11-23 12:14:46 +0800

    chore: remove zh-cn comments
    chore: remove zh-cn comments

--- logger/logger.go
+++ logger/logger.go
@@ -31,13 +31,11 @@ func Init() error {
 	return nil
 }
 
-// 将所有合并
 func tee(cfg *settings.Log, encoder zapcore.Encoder, levelEnabler zapcore.LevelEnabler) (core zapcore.Core, options []zap.Option) {
 	sink := zapWriteSyncer(cfg)
 	return zapcore.NewCore(encoder, sink, levelEnabler), buildOptions(cfg, levelEnabler)
 }
 
-// 构建Option
 func buildOptions(cfg *settings.Log, levelEnabler zapcore.LevelEnabler) (options []zap.Option) {
 	if cfg.Caller {
 		options = append(options, zap.AddCaller())
@@ -49,7 +47,6 @@ func buildOptions(cfg *settings.Log, levelEnabler zapcore.LevelEnabler) (options
 	return
 }
 func zapEncoder(config *settings.Log) zapcore.Encoder {
-	// 新建一个配置
 	encoderConfig := zapcore.EncoderConfig{
 		TimeKey:       "Time",
 		LevelKey:      "Level",
@@ -60,17 +57,11 @@ func zapEncoder(config *settings.Log) zapcore.Encoder {
 		LineEnding:    zapcore.DefaultLineEnding,
 		FunctionKey:   zapcore.OmitKey,
 	}
-	// 自定义时间格式
 	encoderConfig.EncodeTime = CustomTimeFormatEncoder
-	// 日志级别大写
 	encoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
-	// 秒级时间间隔
 	encoderConfig.EncodeDuration = zapcore.SecondsDurationEncoder
-	// 简短的调用者输出
 	encoderConfig.EncodeCaller = zapcore.ShortCallerEncoder
-	// 完整的序列化logger名称
 	encoderConfig.EncodeName = zapcore.FullNameEncoder
-	// 最终的日志编码 json或者console
 	switch config.Encode {
 	case "json":
 		{
@@ -81,7 +72,6 @@ func zapEncoder(config *settings.Log) zapcore.Encoder {
 			return zapcore.NewConsoleEncoder(encoderConfig)
 		}
 	}
-	// 默认console
 	return zapcore.NewConsoleEncoder(encoderConfig)
 }
 func zapLevelEnabler(cfg *settings.Log) zapcore.LevelEnabler {
@@ -97,7 +87,6 @@ func zapLevelEnabler(cfg *settings.Log) zapcore.LevelEnabler {
 	case fatalLevel:
 		return zap.FatalLevel
 	}
-	// 默认Debug级别
 	return zap.DebugLevel
 }
 func zapWriteSyncer(cfg *settings.Log) zapcore.WriteSyncer {
@@ -107,7 +96,6 @@ func zapWriteSyncer(cfg *settings.Log) zapcore.WriteSyncer {
 	}
 
 	if cfg.Writer == writeBoth || cfg.Writer == writeFile {
-		// 添加日志输出器
 		for _, path := range cfg.LogFile.Output {
 			logger := &lumberjack.Logger{
 				Filename:   path,
